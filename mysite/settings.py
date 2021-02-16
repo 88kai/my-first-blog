452d6aa2186a3f4beead51ce9d4248308eb69860
88kai <kai.seshimo@icloud.com>
2021-02-16 22:17:17 +0900

    SECRET_KEY modified

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+try: 
+    from .local_setting import * 
+except ImportError: 
+    pass 
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')^4uwd)ex@svmf778jz$%%c1um27!3x8vtq+v69ha_rl%x_h01'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
